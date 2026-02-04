bba1a2919396dc192a73327354e66b5eb6a0dd90
Elias Shekh <55489470+ThisIsTheWizard@users.noreply.github.com>
2026-02-04 06:40:35 +0600

    chore: delete prisma.config.ts configuration file

--- prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-export default {
-  seed: async () => {
-    await prisma.$executeRaw`ts-node prisma/seed.ts`
-  },
-}
